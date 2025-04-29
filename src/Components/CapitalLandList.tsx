91394b1489ae9d004ff249159effaa0f3a70bf6c
udayviswa <udayviswa23@gmail.com>
2025-04-29 15:08:27 +0530

    Update CapitalLandList.tsx

--- src/Components/CapitalLandList.tsx
+++ src/Components/CapitalLandList.tsx
@@ -3,7 +3,7 @@ export const CapitaLandMallsList = [
     id: 1,
     name: "Aperia Mall",
     link: "https://www.capitaland.com/sg/malls/aperia/en.html",
-    pic: "./public/assets/capitaland/CLmall1.png",
+    pic: "./assets/capitaland/CLmall1.png",
   },
   {
     id: 2,
